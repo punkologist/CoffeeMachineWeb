3fc7d5a449ebfe74732ceabc36a7d5bc11cfe0d4
Daniel Mitchell <daniel.mitchell@migrationmanager.com.au>
2025-09-10 12:30:12 +1000

    added some styling and fixed some issues

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,26 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { CoffeeApiService } from './coffee-api.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: `
-    <button (click)="brewCoffee()">Brew Coffee</button>
-    @if (result()) {
-      <div>{{ result() }}</div>
-    }
-  `,
-  imports: []
+  templateUrl: './home.component.html',
+  imports: [MatButtonModule]
 })
 export class HomeComponent {
-  result = signal<string>('');
+  result = signal<{ message: string; prepared: Date | null }>({ message: '', prepared: null });
 
   constructor(private api: CoffeeApiService) {}
 
   brewCoffee() {
     this.api.brewCoffee().subscribe({
-      next: (res: any) => this.result.set(JSON.stringify(res)),
-      error: (err: { message: string; }) => this.result.set('Error: ' + err.message)
+      next: (res: any) => this.result.set({
+        message: res.message ?? '',
+        prepared: res.prepared ? new Date(res.prepared) : null
+      }),
+      error: (err: { message: string; }) => this.result.set({
+        message: 'Error: ' + err.message,
+        prepared: null
+      })
     });
   }
 }
